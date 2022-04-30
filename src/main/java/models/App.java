c6602d49e74d8806fbc7183bf3bad042f971e216
ynyanchoka <monaryymelda@gmail.com>
2022-04-30 15:06:03 +0300

    add templates

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -1,7 +1,35 @@
 package models;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        staticFileLocation("/public");
+
+       //home
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            return new ModelAndView(new HashMap(), "index.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+        //hero form
+        get( "/heroes/new", (request, response) -> {
+            Map<String, Object>  model= new HashMap <String,Object>();
+            return new ModelAndView( new HashMap (), "heroform.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //squad form
+        get( "/squads/new", (request, response) -> {
+            Map<String, Object>  model= new HashMap <String,Object>();
+            return new ModelAndView( new HashMap (), "squadform.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
