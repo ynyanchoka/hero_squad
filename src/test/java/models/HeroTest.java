2225c6081ecf6f8bb14d0ef389d569eb5c146528
ynyanchoka <monaryymelda@gmail.com>
2022-04-30 17:36:42 +0300

    add test that instatiates several heros

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,14 +1,24 @@
 package models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 
 
 public class HeroTest {
     @Test
-    public void NewHeroObjectGetsCorrectlyCreated_true() {
+    public void NewHeroObjectGetsCorrectlyCreated() {
         Hero hero = new Hero ("Thanos", 20, "Laser eyes","Running");
-        assertEquals (true,hero instanceof Hero);
+        assertTrue (hero instanceof Hero);
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfHero_true() {
+        Hero firstHero = new Hero("Thanos", 20, "Laser eyes", "Running");
+        Hero secondHero = new Hero("Mathai", 40, "Environment conservation", "Poor time keeper");
+        assertTrue(Hero.getAllInstances().contains(firstHero));
+        assertTrue(Hero.getAllInstances().contains(secondHero));;
     }
 }
